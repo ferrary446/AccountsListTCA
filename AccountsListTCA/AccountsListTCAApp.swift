5a26ca15f55b29644d014c02565469ecf3f5b642
Ilya Yushkov <ilyayushkov4@gmail.com>
2025-08-23 13:26:27 +0200

    Initial Commit

---
+++ AccountsListTCA/AccountsListTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccountsListTCAApp.swift
+//  AccountsListTCA
+//
+//  Created by Ilya Yushkov on 23.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct AccountsListTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccountsListTCATests/AccountsListTCATests.swift
@@ -0,0 +1,17 @@
+//
+//  AccountsListTCATests.swift
+//  AccountsListTCATests
+//
+//  Created by Ilya Yushkov on 23.08.2025.
+//
+
+import Testing
+@testable import AccountsListTCA
+
+struct AccountsListTCATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
